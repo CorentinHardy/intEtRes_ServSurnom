9ec13154cbc6c70d96e9777a59919780cbadbb1e
Corentin <corentin.fouyaya@hotmail.fr>
2015-11-02 15:53:44 +0100

    Amelioration Server

--- src/server/Answer.java
+++ src/server/Answer.java
@@ -1,9 +1,20 @@
 package server;
 
+
 import java.io.Serializable;
 
 public class Answer implements Serializable{
-//	public Answer(reponse , aquoi, Exception)
+	Protocol answer, request;
+	String name;
+	String[] surnames;
+	
+	private Answer(Protocal answer, Protocol request){
+		this.answer = answer;
+		this.request = request;
+	}
+	public Answer(Protocol answer, Protocol request, Exception e){
+		Answer(answer, request);
+	}
 //	public Answer(reponse , aquoi, String)
 //	public Answer(reponse , aquoi, String, String)
 }
