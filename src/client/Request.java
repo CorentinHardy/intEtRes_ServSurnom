90614722ddbd476e1f08166fc9629e0243ebddb7
M-Nicolas <meurgues.nicolas@gmail.com>
2015-11-02 15:44:00 +0100

    Ajout de Protocol
    Ajout de l'enum qui contient les noms des protocoles et du constructeur
    de Request.

--- src/client/Request.java
+++ src/client/Request.java
@@ -1,5 +1,13 @@
 package client;
 
-public class Request {
+import common.Protocol;
 
+public class Request {
+	private Protocol protocol;
+	private String name;
+	
+	public Request(Protocol protocol, String name){
+		this.protocol = protocol;
+		this.name = name;
+	}
 }
---
+++ src/common/Protocol.java
@@ -0,0 +1,5 @@
+package common;
+
+public enum Protocol {
+	ADD_NAME, ADD_NICKNAME, GET_NICKNAMES, GET_NAME, REMOVE_NICKNAME, REMOVE_NAME
+}
