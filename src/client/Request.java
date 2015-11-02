bca74ab05fa7284f32329045fce14589edbe3c40
M-Nicolas <meurgues.nicolas@gmail.com>
2015-11-02 15:52:01 +0100

    Amélioration de Request et de Protocol
    ajout des getters dans Request pour récupérer le Protocol, le Nom et le
    Surnom. Ajout d'un constructeur pour le cas où nous avons un surnom.
    Ajout des Protocoles dans le cas d'une réponse.

--- src/client/Request.java
+++ src/client/Request.java
@@ -1,13 +1,34 @@
 package client;
 
+import java.io.Serializable;
+
 import common.Protocol;
 
-public class Request {
+public class Request implements Serializable{
 	private Protocol protocol;
 	private String name;
+	private String nickname;
 	
 	public Request(Protocol protocol, String name){
 		this.protocol = protocol;
 		this.name = name;
+	}
+	
+	public Request(Protocol protocol, String name, String nickname){
+		this.protocol = protocol;
+		this.name = name;
+		this.nickname = nickname;
+	}
+
+	public Protocol getProtocol() {
+		return protocol;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public String getNickname() {
+		return nickname;
 	}
 }
