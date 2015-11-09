5038dfb720bae4f94579d4f21c6fd4500423c9e1
Corentin <corentin.fouyaya@hotmail.fr>
2015-11-09 15:14:00 +0100

    suppression des Sockets, mise a jour du gitignore et du server

--- src/server/Server.java
+++ src/server/Server.java
@@ -1,5 +1,32 @@
 package server;
 
-public class Server {
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.ServerSocket;
+import java.net.UnknownHostException;
 
+public class Server {
+	
+	public static void main(String[] args) {
+		ServerSocket socket = null;
+		
+		Boolean enEcoute = true;
+	
+		// ObjectOutputStream os = null;  // output stream
+		// ObjectInputStream is = null;   // input stream
+		try {
+			socket = new ServerSocket(1234);
+			// os = new ObjectOutputStream(socket.getOutputStream());
+			// is = new ObjectInputStream(socket.getInputStream());
+		} catch (IOException e) {
+			System.err.println("J'ai pas reçu I/O");
+		}
+		
+		while(enEcoute){
+			// accepter une connection
+			// faire un nouveau thread
+		}
+		// socket.close();
+	}
 }
